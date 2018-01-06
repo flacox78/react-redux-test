3737d0e9016c0ab1ce55d7a29586148f7dd12072
fermin osornio mendez <flacox78@hotmail.com>
2018-01-06 15:07:45 +1100

    Project clean up

--- MVC.CatsUI/ClientApp/store/index.ts
+++ MVC.CatsUI/ClientApp/store/index.ts
@@ -1,20 +1,14 @@
-import * as WeatherForecasts from './WeatherForecasts';
-import * as Counter from './Counter';
 import * as Cats from './Cats';
 
 // The top-level state object
 export interface ApplicationState {
-    counter: Counter.CounterState;
-    weatherForecasts: WeatherForecasts.WeatherForecastsState;
     cats: Cats.CatsState;
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
-export const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer,
+export const reducers = {    
     cats: Cats.reducer
 };
 
