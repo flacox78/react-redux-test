fb6b36182f9c2ababb8fd4798d17d4705159680a
fermin osornio mendez <flacox78@hotmail.com>
2018-01-09 21:41:34 +1100

    UI Unit testing (Jest) - Fix

---
+++ MVC.CatsUI/__tests__/reducers/Cats.test.ts
@@ -0,0 +1,20 @@
+﻿import { reducer, CatsState } from '../../ClientApp/store/Cats';
+
+describe('Reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(null, {})).toEqual({ cats: [] })
+    })
+
+    it('should handle REQUEST_CATS', () => {
+        expect(reducer({ cats: [] }, { type: 'REQUEST_CATS' })).toEqual({ cats: [] })
+    })
+
+    it('should handle RECEIVE_CATS', () => {
+        expect(reducer({ cats: [] }, { type: 'RECEIVE_CATS' })).toEqual({ undefined })
+    })
+})
+
+
+//test('adds 1 + 2 to equal 3', () => {
+//    expect(1 + 2).toBe(3);
+//});
